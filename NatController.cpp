ae0dbe9e806e9d3c2b1e5dd429fa9b3f2a7a0aa7
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-06-17 08:22:41 +0200

    Revert "Clamp MSS when tethering to fix broken fragmentation."
    This reverts commit 8da4bcc9f7adc7ac3a5ecd9d1a233373c7b715e3.

--- NatController.cpp
+++ NatController.cpp
@@ -74,9 +74,6 @@ int NatController::setDefaults() {
     if (runCmd(IPTABLES_PATH, "-t nat -F"))
         return -1;
 
-    // May not be supported by kernel, so don't worry about errors.
-    runCmd(IPTABLES_PATH, "-t mangle -F FORWARD");
-
     runCmd(IP_PATH, "rule flush");
     runCmd(IP_PATH, "-6 rule flush");
     runCmd(IP_PATH, "rule add from all lookup default prio 32767");
@@ -176,9 +173,6 @@ int NatController::enableNat(const int argc, char **argv) {
             setDefaults();
             return -1;
         }
-
-        if (runCmd(IPTABLES_PATH, "-t mangle -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu"))
-            LOGW("Unable to set TCPMSS rule (may not be supported by kernel).");
     }
 
     return 0;
